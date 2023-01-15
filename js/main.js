e6382080145633c9e3c484e79d26510b667787d3
RMuhammadamin001 <muhammadrustamov7009@gmail.com>
2023-01-15 11:10:15 +0500

    End

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let prev = document.querySelector('.prev')
 let text = document.querySelector('.text')
 let next = document.querySelector('.next')
 
+
 let page = 1
 let category = ''
 
@@ -19,26 +20,40 @@ btn.forEach(item => {
 async function get(item, count=1){
     category = item
     let data = await fetch(
+        // `https://api.themoviedb.org/3/search/movie?query=monstrous&api_key=c2847ee5ae5ffca9842280e6f3078881&language=en-US&page=1&include_adult=false`
         `https://api.themoviedb.org/3/movie/${item}?api_key=c2847ee5ae5ffca9842280e6f3078881&language=en-US&page=${count}`
         // `https://api.themoviedb.org/3/tv/${item}?api_key=c2847ee5ae5ffca9842280e6f3078881&language=${count}`
         )
     let parseData = await data.json()
+    console.log(parseData);
     render(parseData.results)
 }
+// async function search(){
+//     let dataSearch = await fetch(
+//         `https://api.themoviedb.org/3/search/movie?api_key=c2847ee5ae5ffca9842280e6f3078881&language=en-US&page=1&include_adult=false`
+//         // `https://api.themoviedb.org/3/tv/${item}?api_key=c2847ee5ae5ffca9842280e6f3078881&language=${count}`
+//         )
+//     let parseDataSearch = await dataSearch.json()
+//     console.log(parseDataSearch);
+//     render(parseDataSearch)
+// }
+// search()
 
 function render(arr){
     wrapper.innerHTML=null
 
     for(let i of arr){
         let div = document.createElement('div')
+        let searchMovies = `https://www.themoviedb.org/movie/${i.id}?language=ru`
         div.classList.add('card')
         div.setAttribute('style', 'width:18rem')
         div.innerHTML=`
-            <img src="http://image.tmdb.org/t/p/w500/${i.poster_path}" width={200} class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${i.title}</h5>
-                <p class="card-text">${i.overview}</p>
-            </div>
+        <img src="http://image.tmdb.org/t/p/w500/${i.poster_path}" width={200} class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${i.title}</h5>
+            <p class="card-text">${i.overview}</p>
+            <button type="submit" class="btn btn-primary"><a class="text-white text-none" href=${searchMovies}>submit</a></button>
+        </div>
         `
 
         wrapper.append(div)
